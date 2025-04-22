03b0b4c8dff95c7358e95b461d1e6287acce9eb2
angeles-23 <159773355+angeles-23@users.noreply.github.com>
2025-04-22 21:23:20 +0200

    Add files via upload

---
+++ POO/_04_Utilidades/POO.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package _04_Utilidades;
+
+/**
+ *
+ * @author angam
+ */
+public class POO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        
+        
+    }//MAIN
+    
+    
+}//FIN
