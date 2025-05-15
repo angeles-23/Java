91f3927f1f7a3e72bed92239d36be70f9f6139ed
angeles-23 <159773355+angeles-23@users.noreply.github.com>
2025-05-15 09:53:58 +0200

    Update Factura.java

--- POO/_11_Factura/Factura.java
+++ POO/_11_Factura/Factura.java
@@ -1,6 +1,8 @@
 package _11_Factura;
 
+import _11_Factura.LineaFactura;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
 /**
@@ -11,18 +13,15 @@ public class Factura {
     private String cif;
     private LocalDate fechaFactura;
     private ArrayList<LineaFactura> lineas;
-    private int IVA;
+    private final int IVA = 21;
 
     
-    public Factura(String cif, LocalDate fechaFactura, ArrayList<LineaFactura> lineas, int IVA) {
+    public Factura(String cif, LocalDate fechaFactura) {
         this.cif = cif;
         this.fechaFactura = fechaFactura;
-        this.lineas = lineas;
-        this.IVA = IVA;
+        this.lineas = new ArrayList<>();
     }
 
-    
-    
     
     public String getCif() {
         return cif;
@@ -48,37 +47,60 @@ public class Factura {
         this.lineas = lineas;
     }
 
-    public int getIVA() {
-        return IVA;
-    }
-
-    public void setIVA(int IVA) {
-        this.IVA = IVA;
-    }
     
     
     public void añadirLinea(String ref, String desc, int cantidad, double precioU){
-        
+        LineaFactura nuevaLinea = new LineaFactura(ref, desc, cantidad, precioU);
+        this.lineas.add(nuevaLinea);
     }//
     
     
     private double getSubtotal(){
-        return 0;
+        double sumaTotal = 0;
+        for(LineaFactura l: this.lineas){
+            sumaTotal += l.getImporteLinea();
+        }
+        return sumaTotal;
     }//
     
     
     private double getIva(){
-        return 0;
+        double ivaCalculado = (this.getSubtotal() * this.IVA) / 100;
+        return ivaCalculado;
     }//
     
+    
+    
     private double getTotal(){
-        return 0;
+        return this.getSubtotal() + this.getIva();
     }//
     
     
     public void imprimirFactura(){
+        System.out.println("=".repeat(45));
+        System.out.println(" ".repeat(20) + "FACTURA");
+        System.out.println(String.format("CIF: %s", this.getCif()));
         
+        // Formato fecha
+        DateTimeFormatter formatoFechaConcreta = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        // Buscar la fecha concreta
+        LocalDate fechaBuscada = LocalDate.parse("03/03/2022", formatoFechaConcreta);
+        // Formatear la fecha concreta
+        String fechaFormateada = fechaBuscada.format(formatoFechaConcreta);
+        
+        
+        System.out.println(String.format("Fecha: %s", fechaFormateada));
+        
+        System.out.println(String.format("%-8S %-15S %-12s %-10s", "CANT", "DESCRIPCION", "PRECIO U", "IMPORTE"));
+        System.out.println("-".repeat(45));
+        for(LineaFactura l: this.lineas){
+            l.imprimirLinea();
+        }
+        
+        System.out.println(String.format("\n%35s: %.2f €", "Subtotal", this.getSubtotal()));
+        System.out.println(String.format("%30s %d:   %.2f €", "IVA", this.IVA, getIva()));
+        System.out.println(String.format("%32s:    %.2f€", "TOTAL", this.getTotal()));
+        System.out.println("=".repeat(45));
     }//
-    
-    
+
 }///
